2c8fa0349f3a8e650d349953862c2a2a9a41900f
chomeko3o <kusyopi@gmail.com>
2020-01-26 16:03:28 +0900

    day25　モーダルレスポンシブ完成

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,14 @@
 $(function () {
+  /* スクロール位置を入れる変数を宣言 */
+  var winScrollTop;
   $('#privacy-btn').click(function () {
+    /*スクロールの位置を取得できて、変数に代入*/
+    winScrollTop = $(window).scrollTop();
     $('.privacy__modal').fadeIn();
   });
   $('.privacy--close').click(function () {
     $('.privacy__modal').fadeOut();
+    /* closeボタンを押したら取得した位置まで戻る*/
+    $('body,html').stop().animate({scrollTop:winScrollTop}, 100);
   });
 });
